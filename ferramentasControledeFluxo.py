23c113114c2d159fa40d5c5eb48663cef2c833bc
andresalidia <andresalidia1@gmail.com>
2025-05-23 11:07:12 -0400

    update

---
+++ ferramentasControledeFluxo.py
@@ -0,0 +1,25 @@
+# função if:
+# A função if é usada para executar um bloco de código se uma condição for verdadeira.
+
+# A sintaxe básica é a seguinte:
+# if condição:
+#     # bloco de código a ser executado se a condição for verdadeira
+#else:
+#     # bloco de código a ser executado se a condição for falsa
+#
+# Exemplo:
+idade = 18
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.") 
+    
+    
+# A saída será "Você é maior de idade." porque a condição idade >= 18 é verdadeira.
+
+# Você pode usar operadores de comparação como ==, !=, <, >, <= e >= para comparar valores.
+
+#elif:
+# O elif (abreviação de "else if") é usado para verificar várias condições em uma estrutura if-else.
+# usada para substituir o switch-case em outras linguagens de programação.
+
